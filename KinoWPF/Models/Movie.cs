0e9c7404d632dea5cb2225731b5ca5a7c0829e39
Maciek <maciej.owerczuk@gmail.com>
2017-01-17 13:18:18 +0100

    added entity

---
+++ KinoWPF/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoWPF.Models
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Genre { get; set; }
+        public string Production { get; set; }
+        public string Description { get; set; }
+        public string ImageFileName { get; set; }
+        public DateTime premiereDate { get; set; }
+        public int Length { get; set; }
+        public int NumberOfShows { get; set; }
+    }
+}
